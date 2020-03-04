19fd69ec4549aabbbfa06d923c14d7153bfa07f2
zena <syglee7@gmail.com>
2020-03-04 23:36:59 +0900

    2020.03.04 learn styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled, { createGlobalStyle, css, ThemeProvider } from 'styled-components';
+import theme from './theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle =  createGlobalStyle`
+    body{
+        padding:0;
+        margin:0;
+    }
+`;
+
+const awesomeCard = css`
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+`;
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: pink;
+`;
+
+const Card = styled.div`
+    background-color: red;
+`;
+
+const Button = styled.button`
+    border-radius: 30px;
+    padding: 25px 15px;
+    background-color: ${props => props.theme.successColor};
+`;
+
+
+class App extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <GlobalStyle/>
+                <Container>
+                    <Form />
+                </Container>
+            </ThemeProvider>
+        );
+    }
 }
+
+const Form = () => (
+    <Card><Button>Hello</Button></Card>
+);
+
+
 
 export default App;
